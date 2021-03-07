6cfc9dd4fff86153e1fbdba86eea55d448442560
AF Rakib <38295735+rikitoaf@users.noreply.github.com>
2021-03-07 12:41:48 +0600

    testing template

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
 
-# Create your models here.
+
+
+    
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserRegisterForm
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success (request,f'Account created for {username}!')
+            form.save()
+    else:
+        form = UserRegisterForm(request.POST)
+    return render(request, 'users/templates/register.html', {'form': form})
