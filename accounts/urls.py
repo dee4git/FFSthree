d33aaec6abc70c83cf9351ee3b3182705f16d8d7
Durjoy <57084424+dee4git@users.noreply.github.com>
2021-03-08 10:59:57 +0600

    login fixes::looks nicer than before. design changes. Password reset working.

--- accounts/urls.py
+++ accounts/urls.py
@@ -19,7 +19,6 @@ from django.urls import path
 from . import views as user_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
