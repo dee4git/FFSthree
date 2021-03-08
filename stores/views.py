9a0c40ff43b89ab982e478370d95872286c7a781
TurhanChowdhury <57052897+TurhanChowdhury@users.noreply.github.com>
2021-03-08 23:27:43 +0600

    funstions added to update and  delete store

--- stores/views.py
+++ stores/views.py
@@ -31,6 +31,28 @@ def storeCreate(request):
                   )
 
 
+def storeUpdate(request, id):
+    top = "Edit your store"
+    confirm = "Confirm"
+    confirmation = "Update Store?"
+    cancel = "Cancel"
+    up = Store.objects.get(pk=id)
+    if request.method== "POST":
+        form = forms.Form(request.POST or None, request.FILES, instance=up)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    else:
+        form=forms.Form(instance=up)
+    return render(request, 'create.html',
+                  {"form": form,
+                   "top": top,
+                   "cancel": cancel,
+                   "confirm": confirm,
+                   "confmsg": confirmation,
+                   },
+                  )
 
 def storeView(request, id):
     status = 0
@@ -44,3 +66,7 @@ def storeView(request, id):
                                            "products": products,
                                            })
 
+
+def storeDelete(request, id):
+    Store.objects.get(pk=id).delete()
+    return render("/")
